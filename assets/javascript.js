7fc9cf6e4fdea8d39df9a9880651a12626c0aaf5
Rik Bauwens <rikbauwens1@gmail.com>
2017-08-31 16:48:20 +0200

    add logo to navbar on scrolling

--- assets/javascript.js
+++ assets/javascript.js
@@ -8,9 +8,11 @@ $(window).scroll(function() {
   if (scroll > 5) {
     $('.navbar').addClass('navbar--background')
     $('.navbar-button').addClass('navbar-button--background');
+    $('.navbar__logo').removeClass('navbar__logo--hidden');
   } else {
     $('.navbar').removeClass('navbar--background')
     $('.navbar-button').removeClass('navbar-button--background');
+    $('.navbar__logo').addClass('navbar__logo--hidden');
   }
 })
 
@@ -18,8 +20,11 @@ $(window).scroll(function() {
 
 $(document).on('click', 'a', function(event){
     event.preventDefault();
+    var href = $.attr(this, 'href');
     $('html, body').animate({
-            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-        }, 500);
+            scrollTop: $('[name="' + href.substr(1) + '"]').offset().top
+        }, 500, function() {
+        window.location.hash = '';
+      });
     return false;
 });
