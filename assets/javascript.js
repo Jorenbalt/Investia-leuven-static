fb4f77f7a32a54fb95f8b2224bb7c5a30b4ca9a8
Rik Bauwens <rikbauwens1@gmail.com>
2017-08-31 17:33:40 +0200

    add fade effects

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,11 +1,12 @@
 $(window).scroll(function() {
-  var scroll = $(this).scrollTop();
+  var scrollTop = $(this).scrollTop();
 
   $('.header-content').css({
-    'transform' : 'translate(0px, ' + scroll/3 + 'px)'
+    'transform' : 'translate(0px, ' + scrollTop/3 + 'px)'
   })
 
-  if (scroll > 5) {
+  // Navbar
+  if (scrollTop > 5) {
     $('.navbar').addClass('navbar--background')
     $('.navbar-button').addClass('navbar-button--background');
     $('.navbar__logo').removeClass('navbar__logo--hidden');
@@ -14,6 +15,17 @@ $(window).scroll(function() {
     $('.navbar-button').removeClass('navbar-button--background');
     $('.navbar__logo').addClass('navbar__logo--hidden');
   }
+
+  // Fade ins
+  $('.fade-in').each( function(i){
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},500);
+      }
+  });
+
 })
 
 // SMOOTH SCROLL
